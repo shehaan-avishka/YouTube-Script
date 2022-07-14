a85f5fb97c602a0df415309c50c006154408e688
shehaan-avishka <shehaan.avishka00@gmail.com>
2022-07-14 16:18:39 +0530

    YouTube script 1.0

---
+++ bot.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from datetime import datetime
+import time
+
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+#chrome_options.add_argument("--disable-extensions")
+#chrome_options.add_argument("--disable-gpu")
+#chrome_options.add_argument("--no-sandbox") # linux only
+chrome_options.add_argument("--headless")
+# chrome_options.headless = True # also works
+bot = webdriver.Chrome(options=chrome_options)
+print("\n\n\n\n\n\n")
+print(datetime.today())
+name = input("Hey tell me your favorite song : ")
+#bot = webdriver.Chrome()
+bot.get('https://www.youtube.com')
+print("\nGot it I am trying to do my best, So now I am in youtube")
+time.sleep(2)
+bot.find_element("xpath",'/html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').send_keys(name)
+time.sleep(2)
+bot.find_element("xpath",'/html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/button').click()
+time.sleep(2)
+bot.find_element("xpath",'/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[1]/div[1]/div/div[1]/div/h3/a/yt-formatted-string').click()
+
+print("Hey found your song enjoy\nGood day...!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
