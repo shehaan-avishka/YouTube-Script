38c451ee6d9edb77601ee5c8654e1b00ec32dbb3
shehaan-avishka <shehaan.avishka00@gmail.com>
2022-07-16 10:42:09 +0530

    handled bot quit while playing song

--- bot.py
+++ bot.py
@@ -8,10 +8,13 @@ chrome_options = Options()
 #chrome_options.add_argument("--disable-gpu")
 #chrome_options.add_argument("--no-sandbox") # linux only
 chrome_options.add_argument("--headless")
-# chrome_options.headless = True # also works
+# chrome_options.headless = True # also works options=chrome_options
+
 bot = webdriver.Chrome(options=chrome_options)
 print("\n\n\n\n\n\n")
 print(datetime.today())
+
+
 name = input("Hey tell me your favorite song : ")
 #bot = webdriver.Chrome()
 bot.get('https://www.youtube.com')
@@ -23,5 +26,13 @@ bot.find_element("xpath",'/html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[
 time.sleep(2)
 bot.find_element("xpath",'/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[1]/div[1]/div/div[1]/div/h3/a/yt-formatted-string').click()
 
-print("Hey found your song enjoy\nGood day...!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+print("Hey found your song enjoy\nGood day...!\n\n\n\n")
+
+decission = input("Stop the song [Y]: ").lower()
+print("\n\n\n\n\n\n")
+while decission != "y":
+    decission = input("Stop the song [Y]: ").lower()
+
+bot.quit()
+
 
